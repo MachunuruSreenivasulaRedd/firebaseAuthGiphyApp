f90937e0ae9e188f270627c5ce635243d9ebe7bb
MachunuruSreenivasulaRedd <918296629y@gmail.com>
2023-09-02 16:11:33 +0530

    updated home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,6 +2,7 @@ import React,{useEffect,useState} from 'react'
 import { auth } from '../../firebase';
 import GifsList from '../GifsList' 
 import './index.css'
+import {Redirect} from 'react-router-dom'
 
 const Home = () => {
   const [presentUser,setPresentUser] = useState({isLoggedin: false,uid: '',email: ''});
@@ -23,14 +24,15 @@ const Home = () => {
   },[])
   return (
     <div>
-        {presentUser.isLoggedin && (
+        {presentUser.isLoggedin ? (
         <div>
             <center>
            <h1> Welcome {presentUser.email}</h1>
            <GifsList />
            </center>
         </div>
-        )}
+        ):
+        <Redirect to="/login" />}
     </div>
   )
 }
